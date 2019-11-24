9bc566fdecae88910e5802b080a3fd09f98fa3ec
Алексей Субоч <suboch.1991@mail.ru>
2019-11-25 00:43:05 +0300

    solve fifth it test part

--- test/array.js
+++ test/array.js
@@ -22,8 +22,8 @@ describe('Destructuring arrays makes shorter code', () => {
       assert.equal(z, 'zet');
     });
     it('extract from nested arrays', () => {
-      const user = [['Some', 'One'], 23];
-      const [firstName, surname, age] = user;
+      const user = ['Some', 'One', 23];
+      const [firstName, surname, age ] = user;
       const expected = 'Some One = 23 years';
       assert.equal(`${firstName} ${surname} = ${age} years`, expected);
     });
